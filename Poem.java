42ab1eead812e92acf5cd921b6ff25ff86ee7ddb
skloud <mnorbyn@gmail.com>
2017-07-17 20:13:50 -0500

    Add files via upload

---
+++ Poem.java
@@ -0,0 +1,53 @@
+
+import static java.lang.System.out;
+
+public class Poem
+{
+    public static void main(String[] args)
+    {
+        //
+        out.println("'Twas brillig, and the slithy toves");
+        out.println("   Did gyre and gimble in the wabe;");
+        out.println("All mimsy were the borogoves,");
+        out.println("   And the mome raths outgrabe.\n");
+
+        out.println("\"Beware the Jabberwock, my son!");
+        out.println("   The jaws that bite, the claws that catch!");
+        out.println("Beware the Jubjub bird, and shun");
+        out.println("   The frumious Bandersnatch!\"\n");
+
+        out.println("He took his vorpal sword in hand:");
+        out.println("   Long time the manxome foe he sought -");
+        out.println("So rested he by the Tumtum tree,");
+        out.println("   And stood awhile in thought.\n");
+
+        out.println("And as in uffish thought he stood,");
+        out.println("   The Jabberwock, with eyes of flame,");
+        out.println("Came whiffing through the tulgey wood,");
+        out.println("   And burbled as it came!\n");
+
+        out.println("One, two! One, two! And through and through");
+        out.println("   The vorpal blade went snicker-snack!");
+        out.println("He left it dead, and with its head");
+        out.println("   He went galumphing back.\n");
+
+        out.println("\"And hast thou slain the Jabberwock?");
+        out.println("   Come to my arms, my beamish boy!");
+        out.println("O frabjous day! Callooh! Callay!\"");
+        out.println("   He chortled in his joy.\n");
+
+        out.println("'Twas brillig, and the slithy toves");
+        out.println("   Did gyre and gimble in the wabe;");
+        out.println("All mimsy were the borogoves,");
+        out.println("   And the mome raths outgrabe.\n");
+
+        out.print("\"It seems very pretty,\" she said when she had finished\n");
+        out.print("it, \"but it's RATHER hard to understand!\" (You see\n");
+        out.print("she didn't like to confess, ever to herself, that\n");
+        out.print("she couldn't make it out at all.) \"Somehow it seems\n");
+        out.print("to fill my head with ideas - only I don't exactly\n");
+        out.print("know what they are! However, SOMEBODY killed\n");
+        out.print("SOMETHING: that's clear, at any rate.\"");
+        //
+    }
+}
